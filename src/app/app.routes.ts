23b8f629e22396581114b7555756accd648d73b9
brayan <brayanodev@gmail.com>
2026-07-10 09:43:19 +0200

    derniere modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Main } from './main/main/main';
+import { Features } from './features/features/features';
+import { About } from './about/about/about';
+import { Avis } from './avis/avis/avis';
+import { Pricing } from './pricing/pricing/pricing';
+import { Contact } from './contact/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Main },
+  { path: 'features', component: Features },
+  { path: 'about', component: About },
+  { path: 'review', component: Avis },
+  { path: 'pricing', component: Pricing },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: 'home' },
+];
--- src/app/header/header/header.ts
+++ src/app/header/header/header.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   isOpen: boolean = false;
   titre: string = 'Best App';
-  liste: string[] = ['Home', 'Features', 'About','Review','Pricing','Contact'];
+  liste = [
+    { label: 'Home', route: '/home' },
+    { label: 'Features', route: '/features' },
+    { label: 'About', route: '/about' },
+    { label: 'Review', route: '/review' },
+    { label: 'Pricing', route: '/pricing' },
+    { label: 'Contact', route: '/contact' },
+  ];
 }
